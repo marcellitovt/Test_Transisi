d0dffbae7c52616be3e42b6c7a344d55bc8d6b1d
marcellitovt <marcellitoya@gmail.com>
2021-08-19 06:54:04 +0700

    Migration

---
+++ company-management/database/migrations/2021_08_18_233515_create_table_companies.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableCompanies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tableCompanies', function (Blueprint $table) {
+            $table->id('id_company');
+            $table->string('name');
+            $table->text('logo_path');
+            $table->string('website');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tableCompanies');
+    }
+}
---
+++ company-management/database/migrations/2021_08_18_233621_create_table_employee.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableEmployee extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tableEmployee', function (Blueprint $table) {
+            $table->id('id_employee');
+            $table->string('name');
+            $table->unsignedInteger('id_company')->references('id_company')->on('tableCompanies');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tableEmployee');
+    }
+}
