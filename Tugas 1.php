ca8fd1e5ddbac0101e9c02d302059a5abd7fab84
marcellitovt <marcellitoya@gmail.com>
2021-08-18 12:55:25 +0700

    Tugas 1

--- Tugas 1.php
+++ Tugas 1.php
@@ -1,3 +1,8 @@
+Nilai ujian sebuah kelas tersimpan dalam sebuah string berikut :
+$nilai = “72 65 73 78 75 74 90 81 87 65 55 69 72 78 79 91 100 40 67 77 86”;
+Buatlah sebuah PHP script untuk menentukan (1) nilai rata-rata, (2) 7 nilai tertinggi, (3) 7 nilai terendah dari nilai-nilai di atas.
+
+
 <?php
 // String top Int
 $nilai = "72 65 73 78 75 74 90 81 87 65 55 69 72 78 79 91 100 40 67 77 86";
