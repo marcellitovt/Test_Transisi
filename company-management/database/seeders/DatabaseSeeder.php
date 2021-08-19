27be21b4891cc7c927d53fc997dff9f00fa7703d
marcellitovt <marcellitoya@gmail.com>
2021-08-19 19:29:48 +0700

    seeders user

--- company-management/database/seeders/DatabaseSeeder.php
+++ company-management/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => "Administrator",
+            'email' => 'admin@transisi.id',
+            'password' => bcrypt('transisi')
+        ]);
     }
 }
--- company-management/routes/web.php
+++ company-management/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\CompanyController::class, 'index']);
 Route::resource('/company', App\Http\Controllers\CompanyController::class);
 Route::resource('/employee', App\Http\Controllers\EmployeeController::class);
 
