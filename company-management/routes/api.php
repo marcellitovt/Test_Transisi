d6936d90e6fd794652149c1d1cddd194ff29c138
marcellitovt <marcellitoya@gmail.com>
2021-08-19 19:13:28 +0700

    api read only

--- company-management/routes/api.php
+++ company-management/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/company', [App\Http\Controllers\ApiCompanyController::class, 'index']);
+
+Route::get('/employee', [App\Http\Controllers\ApiEmployeeController::class, 'index']);
