a831b0ecb31503405d78073ebfe5449392e36696
marcellitovt <marcellitoya@gmail.com>
2021-08-19 21:03:45 +0700

    redirect login

--- company-management/routes/web.php
+++ company-management/routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+
+Route::get('/', [App\Http\Controllers\CompanyController::class, 'index']);
 
 Auth::routes();
 
